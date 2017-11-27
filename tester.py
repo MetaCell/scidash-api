c009f271a8927ee345bc18172dbbc610886697bc
NeZanyat <rlatyshenko.dev@gmail.com>
2017-11-27 18:09:35 +0200

    Upload signature

--- tester.py
+++ tester.py
@@ -2,4 +2,4 @@ from scidash_api import client
 
 client_instance = client.ScidashClient()
 
-print(client_instance.login('admin', 'kavabanga'))
+print(client_instance.login('admin', 'kavabanga').upload_object({}))
