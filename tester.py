ae9c545cc461732a5b3be67f1f2e7c858feffd5e
NeZanyat <rlatyshenko.dev@gmail.com>
2017-12-27 17:43:46 +0200

    New attributes for API, new tests, refactored client, new README, changed tests for API

--- tester.py
+++ tester.py
@@ -14,7 +14,8 @@ class ScidashApiTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.client_instance = client.ScidashClient()
+        cls.client_instance = client.ScidashClient(build_info="test_info",
+                hostname="test_host")
 
         with open('json_sample.json') as f:
             cls.json = f.read()
@@ -24,17 +25,27 @@ class ScidashApiTestCase(unittest.TestCase):
                 'password': 'admin_test'
                 }
 
-
     def test_login(self):
         self.client_instance.login(**self.test_user)
 
         self.assertFalse(self.client_instance.token is None)
 
     def test_upload(self):
-        request = self.client_instance.upload_json(self.json)
+        self.client_instance.set_data(self.json)
 
-        self.assertTrue(request.json().get('success'))
+        self.assertIsInstance(self.client_instance.data, dict)
 
-        request = self.client_instance.upload_object(json.loads(self.json))
+        self.client_instance.set_data(json.loads(self.json))
 
-        self.assertTrue(request.json().get('success'))
+        self.assertIsInstance(self.client_instance.data, dict)
+
+        request = self.client_instance.upload()
+
+        response_data = request.json()
+
+        self.assertTrue(response_data.get('success'))
+        self.assertTrue(response_data.get('data').get('test_instance')
+                .get('build_info') == self.client_instance.build_info)
+
+        self.assertTrue(response_data.get('data').get('test_instance')
+                .get('hostname') == self.client_instance.hostname)
