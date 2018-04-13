6096d9751ec6944621508938fea1b45bf57c6904
johnidol <giovanni.idili@gmail.com>
2018-04-13 12:55:45 +0100

    remove slash

--- scidash_api/settings.py
+++ scidash_api/settings.py
@@ -5,7 +5,7 @@ Here you can find all common settings for the library
 '''
 
 CONFIG = {
-    'base_url': 'http://54.177.242.165/',
+    'base_url': 'http://54.177.242.165',
     'upload_url': '/data/upload/{filename}',
     'auth_url': '/api/login/',
     'file_name': 'data.json'
